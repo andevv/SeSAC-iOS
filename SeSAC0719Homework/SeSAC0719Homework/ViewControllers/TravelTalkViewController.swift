58f9294bbcb1daf752cb610f7298ae3103eb903e
andev <andh1006@naver.com>
2025-07-19 18:56:26 +0900

    TRAVEL TALK 화면 임시 완료

--- SeSAC0719Homework/SeSAC0719Homework/ViewControllers/TravelTalkViewController.swift
+++ SeSAC0719Homework/SeSAC0719Homework/ViewControllers/TravelTalkViewController.swift
@@ -8,22 +8,77 @@
 import UIKit
 
 class TravelTalkViewController: UIViewController {
-
+    
+    @IBOutlet var mySearchBar: UISearchBar!
+    @IBOutlet var myCollectionView: UICollectionView!
+    
+    var chatRooms = ChatList.list
+    var filteredRooms: [ChatRoom] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        title = "TRAVEL TALK"
+        filteredRooms = chatRooms
+        
+        configureCollectionView()
+        configureSearchBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureCollectionView() {
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        
+        let nib = UINib(nibName: "TravelTalkCollectionViewCell", bundle: nil)
+        myCollectionView.register(nib, forCellWithReuseIdentifier: "TravelTalkCollectionViewCell")
     }
-    */
+    
+    func configureSearchBar() {
+        mySearchBar.delegate = self
+        mySearchBar.placeholder = "친구 이름을 검색해보세요"
+    }
+}
 
+// MARK: - CollectionView DataSource, Delegate
+extension TravelTalkViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredRooms.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "TravelTalkCollectionViewCell", for: indexPath) as! TravelTalkCollectionViewCell
+        
+        let room = filteredRooms[indexPath.item]
+        cell.configure(with: room)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "ChatViewController") as! ChatViewController
+        vc.chatRoom = filteredRooms[indexPath.item]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = myCollectionView.bounds.width
+        return CGSize(width: width, height: 80)
+    }
+}
+
+// MARK: - SearchBar
+extension TravelTalkViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.trimmingCharacters(in: .whitespaces).isEmpty {
+            filteredRooms = chatRooms
+        } else {
+            filteredRooms = chatRooms.filter { $0.chatroomName.lowercased().contains(searchText.lowercased()) }
+        }
+        myCollectionView.reloadData()
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
 }
---
+++ SeSAC0719Homework/SeSAC0719Homework/Views/TravelTalkCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  TravelTalkCollectionViewCell.swift
+//  SeSAC0719Homework
+//
+//  Created by andev on 7/19/25.
+//
+
+import UIKit
+
+class TravelTalkCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var lastMessageLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        configureProfileImageView()
+        configureNameLabel()
+        configureLastMessageLabel()
+        configureDateLabel()
+        
+        
+    }
+    
+    func configure(with room: ChatRoom) {
+        profileImageView.image = UIImage(named: room.chatroomImage)
+        nameLabel.text = room.chatroomName
+        lastMessageLabel.text = room.chatList.last?.message
+        dateLabel.text = formatDate(room.chatList.last?.date ?? "")
+    }
+    
+    func formatDate(_ string: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        if let date = formatter.date(from: string) {
+            formatter.dateFormat = "yy.MM.dd"
+            return formatter.string(from: date)
+        }
+        return ""
+    }
+    
+    
+    // MARK: - Design
+    
+    func configureProfileImageView() {
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+    }
+    
+    func configureNameLabel() {
+        nameLabel.font = .boldSystemFont(ofSize: 15)
+    }
+    
+    func configureLastMessageLabel() {
+        lastMessageLabel.font = .systemFont(ofSize: 12)
+        lastMessageLabel.textColor = .darkGray
+    }
+    
+    func configureDateLabel() {
+        dateLabel.font = .systemFont(ofSize: 12)
+        dateLabel.textColor = .lightGray
+    }
+
+}
