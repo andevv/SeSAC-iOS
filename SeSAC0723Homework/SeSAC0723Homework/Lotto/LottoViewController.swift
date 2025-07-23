c52b076324a91b4491257a0ca69f6c5b2f602b30
andev <andh1006@naver.com>
2025-07-23 15:02:51 +0900

    Lotto VC 파일 생성

---
+++ SeSAC0723Homework/SeSAC0723Homework/Lotto/LottoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LottoViewController.swift
+//  SeSAC0723Homework
+//
+//  Created by andev on 7/23/25.
+//
+
+import UIKit
+
+class LottoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
