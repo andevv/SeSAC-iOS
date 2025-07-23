9824f9b9f7f6332c678ce9586261ccb1f2c5b4da
andev <andh1006@naver.com>
2025-07-23 14:53:16 +0900

    0723 프로젝트 생성

---
+++ SeSAC0723Homework/SeSAC0723Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC0723Homework
+//
+//  Created by andev on 7/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
