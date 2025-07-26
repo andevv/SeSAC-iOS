dad276be1b2b1cbc5782f80333c80ddea6543870
andev <andh1006@naver.com>
2025-07-27 00:08:10 +0900

    검색 결과 화면 구현

---
+++ SeSAC0723Homework/SeSAC0723Homework/Shopping/Shopping.swift
@@ -0,0 +1,8 @@
+//
+//  Shopping.swift
+//  SeSAC0723Homework
+//
+//  Created by andev on 7/26/25.
+//
+
+import Foundation
---
+++ SeSAC0723Homework/SeSAC0723Homework/Shopping/ShoppingResultCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ShoppingResultCollectionViewCell.swift
+//  SeSAC0723Homework
+//
+//  Created by andev on 7/26/25.
+//
+
+import UIKit
+
+class ShoppingResultCollectionViewCell: UICollectionViewCell {
+    
+}
