1f8f7dd2e860e484ab75d2ce554eb69fef0c1b8b
andev <andh1006@naver.com>
2025-08-13 15:33:31 +0900

    0813프로젝트 Init

---
+++ SeSAC0813Homework/SeSAC0813Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC0813Homework
+//
+//  Created by andev on 8/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
